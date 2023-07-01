6458f12bc0c3d08163db144ac58c419f597daa4a
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-07-01 17:05:39 -0300

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import {View, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Marshal</Text>
+    </View>
+  );
+};
+
+export default App;
