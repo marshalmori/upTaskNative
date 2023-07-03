3cb9672134dd9966c20462c160b23b2f6de1db60
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-07-03 10:46:13 -0300

    Conectar Apollo Client ao projeto React Native

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-/**
- * @format
- */
-
+import React from 'react';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import {ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client';
 
-AppRegistry.registerComponent(appName, () => App);
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/',
+  cache: new InMemoryCache(),
+});
+
+const upTaskApp = () => (
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+);
+
+AppRegistry.registerComponent(appName, () => upTaskApp);
