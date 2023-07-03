cf253d1218a17f5d2ec8e403a02ecf48f7a76205
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-07-03 14:01:31 -0300

    Criar as validações do formulário que cria uma nova conta

--- views/CrearCuenta.js
+++ views/CrearCuenta.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import {View} from 'react-native';
+import React, {useState} from 'react';
 import {
   NativeBaseProvider,
   Box,
@@ -9,9 +8,64 @@ import {
   FormControl,
   Input,
   Button,
+  HStack,
+  Text,
+  IconButton,
+  CloseIcon,
+  Alert,
 } from 'native-base';
+import {useNavigation} from '@react-navigation/native';
+import {useToast} from 'native-base';
 
 const CrearCuenta = () => {
+  const [nombre, guardarNombre] = useState('');
+  const [email, guardarEmail] = useState('');
+  const [password, guardarPassword] = useState('');
+
+  const [mensaje, guardarMensaje] = useState(null);
+
+  // React navigation
+  const navigation = useNavigation();
+
+  // Toast
+  const toast = useToast();
+
+  // Cuando el usuario presiona en criar cuenta
+  const handleSubmit = () => {
+    // validar
+    if (nombre === '' || email === '' || password === '') {
+      // Mostar un error
+      guardarMensaje('Todos los campos son obligatorios');
+      return;
+    }
+    // password al menos 6 caracteres
+    if (password.length < 6) {
+      guardarMensaje('El password debe ser de al menos 6 caracteres.');
+      return;
+    }
+
+    // guardar el usuario
+  };
+
+  // muestra un mensaje toast
+  const mostrarAlerta = () => {
+    return (
+      <Alert w="100%" colorScheme="success" status="error">
+        <VStack space={2} flexShrink={1} w="100%">
+          <HStack
+            flexShrink={1}
+            space={2}
+            alignItems="center"
+            justifyContent="space-between">
+            <HStack space={2} flexShrink={1} alignItems="center">
+              <Text color="coolGray.800">{mensaje}</Text>
+            </HStack>
+          </HStack>
+        </VStack>
+      </Alert>
+    );
+  };
+
   return (
     <NativeBaseProvider>
       <Center w="100%">
@@ -38,19 +92,24 @@ const CrearCuenta = () => {
           <VStack space={3} mt="5">
             <FormControl>
               <FormControl.Label>Nombre</FormControl.Label>
-              <Input />
+              <Input onChangeText={texto => guardarNombre(texto)} />
             </FormControl>
             <FormControl>
               <FormControl.Label>Email</FormControl.Label>
-              <Input />
+              <Input onChangeText={texto => guardarEmail(texto)} />
             </FormControl>
             <FormControl>
               <FormControl.Label>Password</FormControl.Label>
-              <Input type="password" />
+              <Input
+                type="password"
+                onChangeText={texto => guardarPassword(texto)}
+              />
             </FormControl>
-            <Button mt="2" colorScheme="indigo">
-              Sign up
+            <Button mt="2" colorScheme="indigo" onPress={() => handleSubmit()}>
+              Crear Cuenta
             </Button>
+
+            {mensaje && mostrarAlerta()}
           </VStack>
         </Box>
       </Center>
